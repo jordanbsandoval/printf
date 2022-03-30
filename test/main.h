8fd20c105bd39b6bd4b2e06c4bd207eae2be2bc9
jordanbsandoval <1608@holbertonschool.com>
2022-03-30 15:51:47 -0400

    betty

--- test/main.h
+++ test/main.h
@@ -8,11 +8,17 @@
 #include <stdio.h>
 #include <string.h>
 #define PORCENTAJE '%'
-
-typedef struct formato{
+/**
+ *struct formato- structure for search the function to execute
+ *@op: char to search
+ *@f: pointer to function
+ */
+typedef struct formato
+{
 	char *op;
 	int (*f)(va_list);
-}formato;
+}
+formato;
 
 /*prototypes*/
 int _printf(const char *format, ...);
@@ -22,7 +28,7 @@ char *_create_array(const char *);
 void _imprimir(char *, va_list);
 int print_c(va_list);
 int print_s(va_list);
-int print_p();
+int print_p(va_list);
 int print_i(va_list);
 int _atoi(char *);
 
